e32d6a03130155ae1e44efdf2c3421b001bb77f7
kaapomoi <kaapomo@gmail.com>
2023-01-29 14:39:15 +0200

    Add project setup files

---
+++ build.sh
@@ -0,0 +1,27 @@
+base_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+function build(){
+    local build_dir="$base_dir/build/"
+    echo "Creating build directory: $build_dir ";
+
+    rm -rf $build_dir
+    mkdir -p $build_dir
+    cd $build_dir > /dev/null
+
+    echo "Configuring CMake..."
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+    echo "Building...";
+    make -j
+    cd - > /dev/null
+    echo "Copying words.txt to build/"
+    cp words.txt $build_dir
+}
+
+function run(){
+    local build_dir="$base_dir/build/"
+    echo "Running supertyper...";
+
+    cd $build_dir > /dev/null
+    ./supertyper    
+    cd - > /dev/null
+}
